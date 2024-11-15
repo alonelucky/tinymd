b3caafeb3d0e74d8e9a28064e9718e498b688ee9
xiaqb <xiaqiubo123@163.com>
2024-11-15 12:59:02 +0800

    feat: 优化存储机制

--- src/components/CodemirrorEditor/EditorHeader/SaveDropdown.vue
+++ src/components/CodemirrorEditor/EditorHeader/SaveDropdown.vue
@@ -6,7 +6,8 @@ const key_articles = `articles`
 const key___editor_content = `__editor_content`
 const key___editor_content_id = `__editor_content_id`
 
-const storage = (window as any).localforage.config({ size: 2 ** 32 })
+const storage = (window as any).localforage
+storage.config({ size: 2 ** 32 })
 
 interface Article {
   id: string
@@ -18,7 +19,7 @@ interface Article {
 const articlemap: { [x: string]: Article } = {}
 
 onMounted(async () => {
-  const data = (await storage.getItem(key_articles)) || []
+  const data = JSON.parse((await storage.getItem(key_articles)) || `[]`)
   articles.length = 0
   articles.push(...data)
   for (let i = 0; i < data.length; i++) {
@@ -27,11 +28,48 @@ onMounted(async () => {
   }
 })
 
+document.addEventListener(`keydown`, async (e) => {
+  if ((e.ctrlKey || e.metaKey) && e.key === `s`) {
+    e.preventDefault()
+    const current = localStorage.getItem(key___editor_content) ?? ``
+    const currentid = localStorage.getItem(key___editor_content_id) ?? `${Date.now()}${Math.random()}`
+    if (articlemap[currentid]) {
+      articlemap[currentid].title = current.trimStart().slice(0, 30)
+    }
+    else {
+      articles.push({
+        id: currentid,
+        title: current.trimStart().slice(0, 30),
+        date: Date.now(),
+        update: Date.now(),
+      })
+    }
+    await Promise.all([
+      storage.setItem(currentid, current),
+      storage.setItem(key_articles, JSON.stringify(articles)),
+    ])
+  }
+})
+
+function showLoading() {
+  const ele = document.querySelector(`.loading-dialog`) as any
+  if (ele?.style?.display === `none`) {
+    ele.style.display = `flex`
+  }
+}
+
+function closeLoading() {
+  // const ele = document.querySelector('.loading-dialog') as any;
+  // if(ele?.style?.display !== 'none') {
+  //   ele.style.display = 'none';
+  // }
+}
+
 async function hanldeClick(key: string) {
+  showLoading()
   const content = await storage.getItem(key) ?? ``
   const current = localStorage.getItem(key___editor_content) ?? ``
-  const currentid = (await storage.getItem(key___editor_content_id)) ?? `${Date.now()}${Math.random()}`
-  await storage.setItem(currentid, current)
+  const currentid = localStorage.getItem(key___editor_content_id) ?? `${Date.now()}${Math.random()}`
   if (articlemap[currentid]) {
     articlemap[currentid].title = current.trimStart().slice(0, 30)
   }
@@ -44,16 +82,19 @@ async function hanldeClick(key: string) {
     })
   }
 
-  await storage.setItem(key_articles, articles)
+  await Promise.all([
+    storage.setItem(key_articles, JSON.stringify(articles)),
+    storage.setItem(currentid, current),
+  ])
   localStorage.setItem(key___editor_content, content)
-  await storage.setItem(key___editor_content_id, key)
+  localStorage.setItem(key___editor_content_id, key)
   location.reload()
 }
 
 async function newArticle() {
+  showLoading()
   const current = localStorage.getItem(key___editor_content) ?? ``
-  const currentid = (await storage.getItem(key___editor_content_id)) ?? `${Date.now()}${Math.random()}`
-  await storage.setItem(currentid, current)
+  const currentid = localStorage.getItem(key___editor_content_id) ?? `${Date.now()}${Math.random()}`
   if (articlemap[currentid]) {
     articlemap[currentid].title = current.trimStart().slice(0, 30)
   }
@@ -65,7 +106,10 @@ async function newArticle() {
       update: Date.now(),
     })
   }
-  await storage.setItem(key_articles, JSON.stringify(articles))
+  await Promise.all([
+    storage.setItem(currentid, current),
+    storage.setItem(key_articles, JSON.stringify(articles)),
+  ])
   localStorage.setItem(key___editor_content, `## 新文章`)
   location.reload()
 }
@@ -77,11 +121,11 @@ async function newArticle() {
       历史文章
     </MenubarTrigger>
     <MenubarContent align="start">
-      <MenubarItem @click="newArticle()">
+      <MenubarItem @click="() => newArticle().then(closeLoading)">
         <el-icon class="mr-2 h-4 w-4" />
         <span>新文章</span>
       </MenubarItem>
-      <MenubarItem v-for="(item) in articles" :key="item.id" style="max-width: 320px;text-overflow: hidden" @click="hanldeClick(item.id)">
+      <MenubarItem v-for="(item) in articles" :key="item.id" style="max-width: 320px;text-overflow: hidden" @click="() => hanldeClick(item.id).then(closeLoading)">
         <el-icon class="mr-2 h-4 w-4" />
         <span>{{ item.title }} {{ item.date }}</span>
       </MenubarItem>
